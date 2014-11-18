2f215df562a343f716ea9684dd7baa95b5f7b0c9
Bram van Beekveld <bram1409@hotmail.com>
2014-11-18 15:17:52 +0100

    GUI en button koppelingen

--- fontys-cocktailmachine/Form1.Designer.cs
+++ fontys-cocktailmachine/Form1.Designer.cs
@@ -28,12 +28,218 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnNonAlcohol = new System.Windows.Forms.Button();
+            this.btnAlcohol = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnCola = new System.Windows.Forms.Button();
+            this.btnSinaasappelsap = new System.Windows.Forms.Button();
+            this.btnTonic = new System.Windows.Forms.Button();
+            this.btnVodka = new System.Windows.Forms.Button();
+            this.btnMartini = new System.Windows.Forms.Button();
+            this.btnGin = new System.Windows.Forms.Button();
+            this.btnTequilla = new System.Windows.Forms.Button();
+            this.btnRum = new System.Windows.Forms.Button();
+            this.btnPerzik = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.btnDone = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(271, 115);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(283, 111);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Boozelicious";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnNonAlcohol
+            // 
+            this.btnNonAlcohol.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNonAlcohol.Location = new System.Drawing.Point(91, 126);
+            this.btnNonAlcohol.Name = "btnNonAlcohol";
+            this.btnNonAlcohol.Size = new System.Drawing.Size(174, 88);
+            this.btnNonAlcohol.TabIndex = 1;
+            this.btnNonAlcohol.Text = "Non-Alcoholic Drinks";
+            this.btnNonAlcohol.UseVisualStyleBackColor = true;
+            this.btnNonAlcohol.Click += new System.EventHandler(this.btnNonAlcohol_Click);
+            // 
+            // btnAlcohol
+            // 
+            this.btnAlcohol.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAlcohol.Location = new System.Drawing.Point(560, 129);
+            this.btnAlcohol.Name = "btnAlcohol";
+            this.btnAlcohol.Size = new System.Drawing.Size(174, 88);
+            this.btnAlcohol.TabIndex = 1;
+            this.btnAlcohol.Text = "Alcoholic Drinks";
+            this.btnAlcohol.UseVisualStyleBackColor = true;
+            this.btnAlcohol.Click += new System.EventHandler(this.btnAlcohol_Click);
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(718, 428);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(105, 50);
+            this.btnBack.TabIndex = 2;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // btnCola
+            // 
+            this.btnCola.Location = new System.Drawing.Point(197, 19);
+            this.btnCola.Name = "btnCola";
+            this.btnCola.Size = new System.Drawing.Size(143, 90);
+            this.btnCola.TabIndex = 3;
+            this.btnCola.Text = "CocaCola";
+            this.btnCola.UseVisualStyleBackColor = true;
+            this.btnCola.Click += new System.EventHandler(this.btnCola_Click);
+            // 
+            // btnSinaasappelsap
+            // 
+            this.btnSinaasappelsap.Location = new System.Drawing.Point(346, 19);
+            this.btnSinaasappelsap.Name = "btnSinaasappelsap";
+            this.btnSinaasappelsap.Size = new System.Drawing.Size(143, 90);
+            this.btnSinaasappelsap.TabIndex = 3;
+            this.btnSinaasappelsap.Text = "Orange Juice";
+            this.btnSinaasappelsap.UseVisualStyleBackColor = true;
+            this.btnSinaasappelsap.Click += new System.EventHandler(this.btnSinaasappelsap_Click);
+            // 
+            // btnTonic
+            // 
+            this.btnTonic.Location = new System.Drawing.Point(495, 19);
+            this.btnTonic.Name = "btnTonic";
+            this.btnTonic.Size = new System.Drawing.Size(143, 90);
+            this.btnTonic.TabIndex = 3;
+            this.btnTonic.Text = "Tonic";
+            this.btnTonic.UseVisualStyleBackColor = true;
+            this.btnTonic.Click += new System.EventHandler(this.btnTonic_Click);
+            // 
+            // btnVodka
+            // 
+            this.btnVodka.Location = new System.Drawing.Point(346, 232);
+            this.btnVodka.Name = "btnVodka";
+            this.btnVodka.Size = new System.Drawing.Size(143, 90);
+            this.btnVodka.TabIndex = 3;
+            this.btnVodka.Text = "Vodka";
+            this.btnVodka.UseVisualStyleBackColor = true;
+            // 
+            // btnMartini
+            // 
+            this.btnMartini.Location = new System.Drawing.Point(346, 328);
+            this.btnMartini.Name = "btnMartini";
+            this.btnMartini.Size = new System.Drawing.Size(143, 90);
+            this.btnMartini.TabIndex = 3;
+            this.btnMartini.Text = "Martini";
+            this.btnMartini.UseVisualStyleBackColor = true;
+            // 
+            // btnGin
+            // 
+            this.btnGin.Location = new System.Drawing.Point(495, 232);
+            this.btnGin.Name = "btnGin";
+            this.btnGin.Size = new System.Drawing.Size(143, 90);
+            this.btnGin.TabIndex = 3;
+            this.btnGin.Text = "Gin";
+            this.btnGin.UseVisualStyleBackColor = true;
+            // 
+            // btnTequilla
+            // 
+            this.btnTequilla.Location = new System.Drawing.Point(495, 328);
+            this.btnTequilla.Name = "btnTequilla";
+            this.btnTequilla.Size = new System.Drawing.Size(143, 90);
+            this.btnTequilla.TabIndex = 3;
+            this.btnTequilla.Text = "Tequilla";
+            this.btnTequilla.UseVisualStyleBackColor = true;
+            // 
+            // btnRum
+            // 
+            this.btnRum.Location = new System.Drawing.Point(197, 232);
+            this.btnRum.Name = "btnRum";
+            this.btnRum.Size = new System.Drawing.Size(143, 90);
+            this.btnRum.TabIndex = 3;
+            this.btnRum.Text = "Rum";
+            this.btnRum.UseVisualStyleBackColor = true;
+            // 
+            // btnPerzik
+            // 
+            this.btnPerzik.Location = new System.Drawing.Point(197, 328);
+            this.btnPerzik.Name = "btnPerzik";
+            this.btnPerzik.Size = new System.Drawing.Size(143, 90);
+            this.btnPerzik.TabIndex = 3;
+            this.btnPerzik.Text = "Peach Liquor ";
+            this.btnPerzik.UseVisualStyleBackColor = true;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.progressBar.Location = new System.Drawing.Point(40, 428);
+            this.progressBar.Maximum = 150;
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(656, 50);
+            this.progressBar.Step = 1;
+            this.progressBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar.TabIndex = 4;
+            // 
+            // btnDone
+            // 
+            this.btnDone.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDone.Location = new System.Drawing.Point(718, 272);
+            this.btnDone.Name = "btnDone";
+            this.btnDone.Size = new System.Drawing.Size(105, 50);
+            this.btnDone.TabIndex = 2;
+            this.btnDone.Text = "Done";
+            this.btnDone.UseVisualStyleBackColor = true;
+            this.btnDone.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(835, 490);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.btnTequilla);
+            this.Controls.Add(this.btnSinaasappelsap);
+            this.Controls.Add(this.btnGin);
+            this.Controls.Add(this.btnPerzik);
+            this.Controls.Add(this.btnMartini);
+            this.Controls.Add(this.btnTonic);
+            this.Controls.Add(this.btnRum);
+            this.Controls.Add(this.btnVodka);
+            this.Controls.Add(this.btnCola);
+            this.Controls.Add(this.btnDone);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnAlcohol);
+            this.Controls.Add(this.btnNonAlcohol);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnNonAlcohol;
+        private System.Windows.Forms.Button btnAlcohol;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnCola;
+        private System.Windows.Forms.Button btnSinaasappelsap;
+        private System.Windows.Forms.Button btnTonic;
+        private System.Windows.Forms.Button btnVodka;
+        private System.Windows.Forms.Button btnMartini;
+        private System.Windows.Forms.Button btnGin;
+        private System.Windows.Forms.Button btnTequilla;
+        private System.Windows.Forms.Button btnRum;
+        private System.Windows.Forms.Button btnPerzik;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button btnDone;
     }
 }
 
--- fontys-cocktailmachine/Form1.cs
+++ fontys-cocktailmachine/Form1.cs
@@ -12,10 +12,167 @@ namespace fontys_cocktailmachine
 {
     public partial class Form1 : Form
     {
+        List<int> customRecept;
         public Form1()
         {
             InitializeComponent();
-            MessageBox.Show("Lolololololll");
+            customRecept = new List<int>();
+            btnStart.Visible = true;
+            btnAlcohol.Visible = false;
+            btnNonAlcohol.Visible = false;
+            btnBack.Visible = false;
+            btnSinaasappelsap.Visible = false;
+            btnCola.Visible = false;
+            btnTonic.Visible = false;
+
+            btnVodka.Visible = false;
+            btnGin.Visible = false;
+            btnMartini.Visible = false;
+            btnTequilla.Visible = false;
+            btnRum.Visible = false;
+            btnPerzik.Visible = false;
+
+            btnDone.Visible = false;
         }
+        private void fill()
+        {
+            if (progressBar.Value + 25 <= 150)
+            {
+                progressBar.Value = progressBar.Value + 25;
+            }
+            else
+            {
+                progressBar.Value = progressBar.Maximum;
+            }
+            btnDone.Visible = true;
+            if (progressBar.Value == 100)
+            {
+                klaar();
+            }
+        }
+        
+        private void homeScreen()
+            {
+            btnStart.Visible = true;
+            btnAlcohol.Visible = false;
+            btnNonAlcohol.Visible = false;
+            btnBack.Visible = false;
+            btnSinaasappelsap.Visible = false;
+            btnCola.Visible = false;
+            btnTonic.Visible = false;
+
+            btnVodka.Visible = false;
+            btnGin.Visible = false;
+            btnMartini.Visible = false;
+            btnTequilla.Visible = false;
+            btnRum.Visible = false;
+            btnPerzik.Visible = false;
+
     }
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            btnAlcohol.Visible = true;
+            btnAlcohol.Enabled = true;
+            btnNonAlcohol.Visible = true;
+            btnNonAlcohol.Enabled = true;
+            btnStart.Visible = false;
+            btnBack.Visible = true;
+            btnSinaasappelsap.Visible = false;
+            btnCola.Visible = false;
+            btnTonic.Visible = false;
+
+            btnVodka.Visible = false;
+            btnGin.Visible = false;
+            btnMartini.Visible = false;
+            btnTequilla.Visible = false;
+            btnRum.Visible = false;
+            btnPerzik.Visible = false;
+            progressBar.Value = 0;
+            progressBar.Visible = true;
+
+
+           
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            if (btnAlcohol.Visible)
+            {
+                homeScreen();
+            }
+            if (btnAlcohol.Enabled == false || btnNonAlcohol.Enabled == false)
+            {
+                btnStart_Click(sender, e);
+            }
+        }
+
+        private void btnNonAlcohol_Click(object sender, EventArgs e)
+        {
+            btnNonAlcohol.Enabled = false;
+            btnSinaasappelsap.Visible = true;
+            btnCola.Visible = true;
+            btnTonic.Visible = true;
+
+            btnAlcohol.Enabled = true;
+            btnVodka.Visible = false;
+            btnGin.Visible = false;
+            btnMartini.Visible = false;
+            btnTequilla.Visible = false;
+            btnRum.Visible = false;
+            btnPerzik.Visible = false;
+
+        }
+
+        private void btnAlcohol_Click(object sender, EventArgs e)
+        {
+            btnAlcohol.Enabled = false;
+            btnVodka.Visible = true;
+            btnGin.Visible = true;
+            btnMartini.Visible = true;
+            btnTequilla.Visible = true;
+            btnRum.Visible = true;
+            btnPerzik.Visible = true;
+
+            btnNonAlcohol.Enabled = true;
+            btnSinaasappelsap.Visible = false;
+            btnCola.Visible = false;
+            btnTonic.Visible = false;
+            
+           }
+
+
+
+
+        private void klaar()
+        {
+            //code naar arduino
+
+        }
+
+        private void btnCola_Click(object sender, EventArgs e)
+        { 
+            customRecept.Add(0);
+            fill();
+        }
+
+        private void btnSinaasappelsap_Click(object sender, EventArgs e)
+        {
+            customRecept.Add(1);
+            fill();
+        }
+
+        private void btnTonic_Click(object sender, EventArgs e)
+        {
+            customRecept.Add(2);
+            fill();
+        }
+
+
+
+
+
+
+
+
+     }
 }
