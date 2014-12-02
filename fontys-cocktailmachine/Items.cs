6588f9534a712867df6cda33a98ff0305fae1385
Requinard <d.diks@mail.blastedsolutions.com>
2014-12-02 11:54:12 +0100

    Added Items class

---
+++ fontys-cocktailmachine/Items.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fontys_cocktailmachine
+{
+    class Items
+    {
+        private string _name;
+        private string _id;
+
+        public Items(string name, string id)
+        {
+            _name = name;
+            _id = id;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        public string Id
+        {
+            get { return _id; }
+        }
+    }
+}
